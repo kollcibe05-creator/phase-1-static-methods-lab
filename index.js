f9fe0730698374c0d337da5dce5fe92c75b4d154
Collins Kibet <kollcibe05@gmail.com>
2026-03-19 14:03:11 +0300

    solved -- regex and exceptions

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
   //add static methods here
+    static capitalize (string) {
+        return string.charAt(0).toUpperCase() + string.slice(1)
+    }
+    static sanitize(string) {
+        return string.replace(/[^A-Za-z0-9\s-']+/g, "");
+    }
+    static titleize (string) {
+        const exceptions = new Set(["a", "the", "an", "but", "of", "and", "or", "for", "at", "by", "from"])
+        return string.toLowerCase().split(" ").map((word, index) => {
+          if (index === 0 || !exceptions.has(word)){
+              return word.charAt(0).toUpperCase() + word.slice(1)
+              
+          } 
+          return word
+        }).join(" ")
+
+    }
+
 }
